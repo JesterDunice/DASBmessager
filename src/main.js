92602e50f4afbce1328d67335067127971f61185
JesterDunice <nikita.dudin@gmail.com>
2017-03-20 15:41:10 +0300

    1st iter, switchLogin scene, Sign Up scene layout

---
+++ src/main.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Navigator,
+  StyleSheet,
+  AppState,
+  Platform
+} from 'react-native';
+
+import Login from './pages/login';
+import OpenChannel from './pages/openChannel';
+import CreateChannel from './pages/createChannel';
+import Chat from './pages/chat';
+import Participants from './pages/participants';
+import BlockList from './pages/blockList';
+import GroupChannel from './pages/groupChannel';
+import InviteUser from './pages/inviteUser';
+import Members from './pages/members';
+import SwitchLogin from './pages/switchLogin';
+import SignUp from './pages/signUp'
+
+import {APP_ID} from './consts'
+import SendBird from 'sendbird'
+var sb = null;
+
+
+
+var ROUTES = {
+  login: Login,
+  openChannel: OpenChannel,
+  createChannel: CreateChannel,
+  chat: Chat,
+  participants: Participants,
+  blockList: BlockList,
+  groupChannel: GroupChannel,
+  inviteUser: InviteUser,
+  members: Members,
+  switchlogin: SwitchLogin,
+  signup: SignUp,
+};
+
+export default class Main extends Component {
+
+  componentDidMount() {
+    sb = new SendBird({appId: APP_ID});
+
+    AppState.addEventListener('change', function(currentAppState){
+      if (currentAppState === 'active') {
+        console.log('foreground');
+        sb.setForegroundState();
+      } else if (currentAppState === 'background') {
+        console.log('background');
+        sb.setBackgroundState();
+      }
+    });
+    var Notifications = require('react-native-push-notification');
+    Notifications.configure({
+        onRegister: function(token) {
+            if (Platform.OS === 'ios') {
+              sb.registerAPNSPushTokenForCurrentUser(token['token'], function(result, error){
+                console.log("registerAPNSPushTokenForCurrentUser");
+                console.log(result);
+              });
+            } else {
+              sb.registerGCMPushTokenForCurrentUser(token['token'], function(result, error){
+                console.log("registerAPNSPushTokenForCurrentUser");
+                console.log(result);
+              });
+            }
+        },
+
+        onNotification: function(notification) {
+            console.log( 'NOTIFICATION:', notification );
+        },
+
+        // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications) 
+        senderID: "984140644677",
+
+        // IOS ONLY (optional): default: all - Permissions to register.
+        permissions: {
+            alert: true,
+            badge: true,
+            sound: true
+        },
+
+        // Should the initial notification be popped automatically
+        // default: true
+        popInitialNotification: true,
+
+        /**
+          * (optional) default: true
+          * - Specified if permissions (ios) and token (android and ios) will requested or not,
+          * - if not, you must call PushNotificationsHandler.requestPermissions() later
+          */
+        requestPermissions: true,
+    });
+    
+  }
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{name: 'signup'}}
+        renderScene={this._renderScene}
+        configureScene={() => {return Navigator.SceneConfigs.FloatFromRight;}}
+        style={styles.container}
+      />
+    )
+  }
+
+  _renderScene(route, navigator) {
+    var Component = ROUTES[route.name];
+    return <Component route={route} navigator={navigator} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
---
+++ src/pages/signUp.js
@@ -0,0 +1,272 @@
+/**
+ * Created by dunice on 20.03.17.
+ */
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Platform,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableHighlight
+} from 'react-native'
+
+const LoginView = Platform.select({
+  ios: () => KeyboardAvoidingView,
+  android: () => View,
+})();
+
+import TopBar from '../components/topBar';
+import Button from '../components/button'
+import SendBird from 'sendbird'
+var sb = null;
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userNickname: '',
+      userEmail: '',
+      password: '',
+      passwordConfirm: '',
+      registerLabel: 'Register',
+      errorMessage: ''
+    };
+    this._onPressRegister = this._onPressRegister.bind(this);
+    this._alreadySignUp = this._alreadySignUp.bind(this);
+  }
+
+  _onPressRegister() {
+    Keyboard.dismiss();
+
+    if (this.state.userNickname.trim().length == 0 || this.state.userEmail.trim().length == 0 || this.state.password.trim().length == 0 || this.state.passwordConfirm.trim().length == 0) {
+      this.setState({
+        userNickname: '',
+        userEmail: '',
+        password: '',
+        passwordConfirm: '',
+        errorMessage: 'Value is required and can\'t be empty'
+      });
+      return;
+    }
+
+    if (!(this.state.password === this.state.passwordConfirm)) {
+      this.setState({
+        userNickname: '',
+        userEmail: '',
+        password: '',
+        passwordConfirm: '',
+        errorMessage: 'Passwords don\'t match'
+      });
+      return;
+    }
+
+    let regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
+    if (regExp.test(this.state.userNickname) || regExp.test(this.state.password) || regExp.test(this.state.passwordConfirm)) {
+      this.setState({
+        userNickname: '',
+        userEmail: '',
+        password: '',
+        passwordConfirm: '',
+        errorMessage: 'Please use only alphanumeric characters.'
+      });
+      return;
+    }
+
+    let regExpMail = /[\{\}\[\]\/?,;:|\)*~`!^\+<>\#$%&\\\=\(\'\"]/gi
+    if (regExpMail.test(this.state.userEmail)) {
+      this.setState({
+        userNickname: '',
+        userEmail: '',
+        password: '',
+        passwordConfirm: '',
+        errorMessage: 'Please use only alphanumeric characters.'
+      });
+      return;
+    }
+
+    sb = SendBird.getInstance();
+    var _SELF = this;
+    sb.connect(_SELF.state.userNickname, function (user, error) {
+      if (error) {
+        _SELF.setState({
+          userNickname: '',
+          userEmail: '',
+          password: '',
+          passwordConfirm: '',
+          errorMessage: 'Register Error'
+        });
+        console.log(error);
+        return;
+      }
+
+      if (Platform.OS === 'ios') {
+        if (sb.getPendingAPNSToken()){
+          sb.registerAPNSPushTokenForCurrentUser(sb.getPendingAPNSToken(), function(result, error){
+            console.log("APNS TOKEN REGISTER AFTER LOGIN");
+            console.log(result);
+          });
+        }
+      } else {
+        if (sb.getPendingGCMToken()){
+          sb.registerGCMPushTokenForCurrentUser(sb.getPendingGCMToken(), function(result, error){
+            console.log("GCM TOKEN REGISTER AFTER LOGIN");
+            console.log(result);
+          });
+        }
+      }
+
+      sb.updateCurrentUserInfo(_SELF.state.username, '', function(response, error) {
+        _SELF.setState({
+          buttonDisabled: false,
+          connectLabel: 'DISCONNECT',
+          errorMessage: ''
+        });
+      });
+    });
+  }
+
+  _alreadySignUp(){
+    this.props.navigator.push({name: 'login'})
+  }
+
+  _onPressDisconnect() {
+    sb.disconnect();
+    this.setState({
+      userId: '',
+      username: '',
+      errorMessage: '',
+      buttonDisabled: true,
+      connectLabel: 'CONNECT'
+    });
+  }
+
+  _onBackPress() {
+    this.props.navigator.pop();
+  }
+
+  _buttonStyle() {
+    return {
+      backgroundColor: '#66aa33',
+      underlayColor: '#448833',
+      borderColor: '#66aa33',
+      disabledColor: '#ababab',
+      textColor: '#ffffff'
+    }
+  }
+
+  render() {
+    return (
+      <LoginView behavior='padding' style={styles.container} >
+
+        <View style={styles.topbar}>
+          <TopBar
+            onBackPress={this._onBackPress.bind(this)}
+            title={''}
+          />
+        </View>
+
+        <View style={styles.loginContainer}>
+          <TextInput
+            style={styles.input}
+            value={this.state.userNickname}
+            onChangeText={(text) => this.setState({userNickname: text})}
+            onSubmitEditing={Keyboard.dismiss}
+            placeholder={'Enter User Nickname'}
+            maxLength={12}
+            multiline={false}
+          />
+          <TextInput
+            style={[styles.input, {marginTop: 10}]}
+            value={this.state.userEmail}
+            onChangeText={(text) => this.setState({userEmail: text})}
+            onSubmitEditing={Keyboard.dismiss}
+            placeholder={'Enter User Email'}
+            maxLength={30}
+            multiline={false}
+          />
+          <TextInput
+            style={[styles.input, {marginTop: 10}]}
+            value={this.state.password}
+            onChangeText={(text) => this.setState({password: text})}
+            onSubmitEditing={Keyboard.dismiss}
+            placeholder={'Enter User Password'}
+            maxLength={12}
+            multiline={false}
+          />
+          <TextInput
+            style={[styles.input, {marginTop: 10}]}
+            value={this.state.passwordConfirm}
+            onChangeText={(text) => this.setState({passwordConfirm: text})}
+            onSubmitEditing={Keyboard.dismiss}
+            placeholder={'Confirm User Password'}
+            maxLength={12}
+            multiline={false}
+          />
+
+          <Button
+            text={this.state.registerLabel}
+            style={this._buttonStyle()}
+            onPress={this._onPressRegister}
+          />
+
+          <Text style={styles.errorLabel}>{this.state.errorMessage}</Text>
+
+          <TouchableHighlight
+                              style={{justifyContent: 'center', alignItems: 'center'}}
+                              onPress={this._alreadySignUp}
+                              underlayColor={'#fff'}
+          >
+            <Text style={styles.registeredLabel}>Already signed up?</Text>
+          </TouchableHighlight>
+
+        </View>
+      </LoginView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loginContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  topbar: {
+    height: 60,
+    flexDirection: 'row'
+  },
+  input: {
+    width: 250,
+    color: '#555555',
+    padding: 10,
+    height: 50,
+    borderColor: '#66aa33',
+    borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff'
+  },
+  errorLabel: {
+    color: '#ff0200',
+    fontSize: 13,
+    marginTop: 10,
+    width: 250
+  },
+  registeredLabel:{
+    color: '#000',
+    fontSize: 13,
+    marginTop: 40,
+    width: 250,
+    alignSelf: 'center',
+    textAlign: 'center',
+  }
+});
+
